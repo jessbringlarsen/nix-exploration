839f8c7f0139f168c3a8c0aca909440030c441ab
jla <jess+github@bring-larsen.dk>
2022-10-10 21:17:57 +0200

    cleanup

--- shell.nix
+++ shell.nix
@@ -3,12 +3,10 @@
 		name = "My Awesome Shell"; 
 		buildInputs = with pkgs; [
 			figlet
-			cowsay
 			htop
 		];
 
 		shellHook = ''
-                        unset PROMPT_COMMAND
 			echo "Welcome!"
 		'';
 }
